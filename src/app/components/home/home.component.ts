c33f76358e01117367e59ccaf681d579373e2abc
knevctt <knevctt@outlook.com>
2024-10-30 19:14:43 -0300

    commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,27 +12,38 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  books: any[] = [
+  books = [
     {
       title: 'Harry Potter e a Pedra Filosofal',
       author: 'J. K. Rowling',
-      coverImage: '../../../assets/images/1.png'
+      coverImage: 'https://i0.wp.com/animagos.com.br/wp-content/uploads/2017/07/livro-01_capa.jpg?ssl=1'
     },
     {
       title: 'Harry Potter e a Câmara Secreta',
       author: 'J. K. Rowling',
-      coverImage: 'assets/images/2.png'
+      coverImage: 'https://m.media-amazon.com/images/I/81jbivNEVML._SL1500_.jpg'
     },
     {
       title: 'Harry Potter e o Cálice de Fogo',
       author: 'J. K. Rowling',
-      coverImage: 'assets/images/book3.jpg'
+      coverImage: 'https://i0.wp.com/animagos.com.br/wp-content/uploads/2017/07/livro-04_capa.jpg?ssl=1'
     },
     {
       title: 'Harry Potter e a Ordem da Fênix',
       author: 'J. K. Rowling',
-      coverImage: 'assets/images/book4.jpg'
+      coverImage: 'https://i0.wp.com/animagos.com.br/wp-content/uploads/2017/07/livro-05_capa.jpg?ssl=1'
+    },
+    {
+      title: 'Harry Potter e o Enigma do Principe',
+      author: 'J. K. Rowling',
+      coverImage: 'https://i0.wp.com/animagos.com.br/wp-content/uploads/2017/07/livro-06_capa.jpg?ssl=1'
+    },
+    {
+      title: 'Harry Potter e as Reliquias da Morte',
+      author: 'J. K. Rowling',
+      coverImage: 'https://i0.wp.com/animagos.com.br/wp-content/uploads/2017/07/livro-07_capa.jpg?ssl=1'
     }
     
   ];
 }
+
