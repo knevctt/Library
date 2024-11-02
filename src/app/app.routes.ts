0f38bed16fb6e643b3ef96c4da798a3ac30e88d3
ThiagoZanardi05 <thiagozanardi05@gmail.com>
2024-11-02 15:56:07 -0300

    Login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-
+    {path: '', component: LoginComponent},
     {path: '', component: HomeComponent},
     {path: 'home',component: HomeComponent},
 ];
@@ -14,3 +15,5 @@ export const routes: Routes = [
     exports: [RouterModule]
   })
   export class AppRoutingModule {}
+
+  
