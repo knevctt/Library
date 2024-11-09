2caf4472b761a222b3bbbafc1ee37f322a1a55b2
PauloGabrielX <costacosta1353@gmail.com>
2024-11-09 18:59:17 -0300

    pagina inicial humilde

--- src/app/components/initial-page/initial-page.component.ts
+++ src/app/components/initial-page/initial-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { HomeComponent } from '../home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-initial-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent,HomeComponent,RouterModule],
   templateUrl: './initial-page.component.html',
   styleUrl: './initial-page.component.scss'
 })
